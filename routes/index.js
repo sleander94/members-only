6f6cd44716b7d60a010210cb5c908ca5a57ed1b0
Stephen Leander <stephen.leander1@gmail.com>
2022-07-12 18:01:48 +0200

    Import user_controller, add routes for log-in, log-out, & sign-up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const user_controller = require('../controllers/userController');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.json();
+router.get('/', function (req, res) {
+  res.render('index');
 });
+router.get('/sign-up', user_controller.sign_up_form_get);
+router.post('/sign-up', user_controller.sign_up_form_post);
+router.get('/log-in', user_controller.log_in_form_get);
+router.post('/log-in', user_controller.log_in_form_post);
+router.post('/log-out', user_controller.log_out_post);
 
 module.exports = router;
