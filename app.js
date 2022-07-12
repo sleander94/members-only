474c0ddaa7e3a95e1bd9aa458ca83f8419a2c52a
Stephen Leander <stephen.leander1@gmail.com>
2022-07-12 18:47:03 +0200

    Move res.locals.currentUser declaration to end of app.use functions

--- app.js
+++ app.js
@@ -59,15 +59,15 @@ passport.deserializeUser(function (id, done) {
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
-});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
