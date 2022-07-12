4e31273ab6862f4c98960760e23b90bc4b763309
Stephen Leander <stephen.leander1@gmail.com>
2022-07-12 21:04:44 +0200

    Import message_controller & add routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const user_controller = require('../controllers/userController');
+const message_controller = require('../controllers/messageController');
 
-router.get('/', function (req, res) {
-  res.render('index');
-});
 router.get('/sign-up', user_controller.sign_up_form_get);
 router.post('/sign-up', user_controller.sign_up_form_post);
 router.get('/log-in', user_controller.log_in_form_get);
@@ -12,5 +10,9 @@ router.post('/log-in', user_controller.log_in_form_post);
 router.post('/log-out', user_controller.log_out_post);
 router.get('/join', user_controller.member_join_form_get);
 router.post('/join', user_controller.member_join_form_post);
+
+router.get('/', message_controller.index);
+router.get('/new-message', message_controller.new_message_form_get);
+router.post('/new-message', message_controller.new_message_form_post);
 
 module.exports = router;
