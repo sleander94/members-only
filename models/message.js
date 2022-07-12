395d1e4a26a69fc6eb2741aaa5007eb88a1a6fcb
Stephen Leander <stephen.leander1@gmail.com>
2022-07-12 06:01:52 +0200

    Initial commit

---
+++ models/message.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+  author: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  isMember: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
