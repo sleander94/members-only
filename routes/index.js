bf0650753eef94e562a697f8e40d8a2f293c0667
Stephen Leander <stephen.leander1@gmail.com>
2022-07-12 18:41:17 +0200

    Add member-join-form get & post routes

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,7 @@ router.post('/sign-up', user_controller.sign_up_form_post);
 router.get('/log-in', user_controller.log_in_form_get);
 router.post('/log-in', user_controller.log_in_form_post);
 router.post('/log-out', user_controller.log_out_post);
+router.get('/join', user_controller.member_join_form_get);
+router.post('/join', user_controller.member_join_form_post);
 
 module.exports = router;
